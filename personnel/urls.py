a53dc774eab04eb1b8b65c1fdb1a3620905de77a
iso <ismailkurtlojistik@gmail.com>
2023-01-14 14:20:09 +0300

    last commit

--- personnel/urls.py
+++ personnel/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import DepartmentView
-
+from .views import (
+    DepartmentView,
+    PersonnelListCreateView,
+    PersonalGetUpdateDelete,
+    DepartmentPersonnelView,
+    Custom
+)
 
 urlpatterns = [
     path("department/", DepartmentView.as_view()),
+    path("personnel/", PersonnelListCreateView.as_view()),
+    path("personnel/<int:pk>/", PersonalGetUpdateDelete.as_view()),
+    path("department/<str:name>/", Custom.as_view()),
+    # path("personnel/<str:department>/", DepartmentPersonnelView.as_view()),
 ]
